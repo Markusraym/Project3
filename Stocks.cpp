accd4a89ed1b62fd68b7586e604d718a19ddf8a1
Markusraym <92881502+Markusraym@users.noreply.github.com>
2023-04-19 16:57:27 -0400

    Update Stocks.cpp
    Updated some of the values from the csv file

--- Stocks.cpp
+++ Stocks.cpp
@@ -1,48 +1,56 @@
 #include "Stocks.h"
 
-void StockInfo::LoadStocks() {
-
-    std::map<std::string, StockInfo> stockMap;
+ std::map<std::string, StockInfo> stockMap;
 
     // Open the CSV file
     std::ifstream file("stocks.csv");
     if (!file.is_open()) {
         std::cout << "Failed to open CSV file." << std::endl;
-
+        return 1;
     }
 
     std::string line;
     while (std::getline(file, line))
     {
         std::istringstream iss(line);
-        std::string symbol;
-        double price;
-        int quantity;
+        string symbol;
+        double open;
+        double high;
+        double low;
+        double close;
+        long long int volume;
+        double dividends;
         // Read stock information from CSV
-        if (iss >> symbol >> price >> quantity)
+        if (iss >> symbol >> open >> high >> low >> close >> volume >> dividends) 
         {
             StockInfo stock; //create a stock object
             stock.symbol = symbol; //Assign the name/symbol of the stock
-            stock.price = price; //Assign the price of one share of the stock
-            stock.quantity = quantity; //Assign the quantity, maybe? Maybe risk?
+            stock.open = open; 
+            stock.high = high;
+            stock.low = low;
+            stock.close = close;
+            stock.volume = volume;
+            stock.dividends = dividends;
 
             // Store stock information in the map with stock name as the key
             stockMap[symbol] = stock;
         }
-        else
+        else 
         {
-            std::cout << "Failed to read stock information from line: " << line << std::endl;
+            std::cout << "Failed to read stock information from line: " << line << endl;
         }
     }
 
-    stockList = stockMap;
-
     // Access stock information from the map
-    for (const auto& pair : stockMap)
+    for (const auto& pair : stockMap) 
     {
-        std::cout << "Stock name: " << pair.first << std::endl;
-        std::cout << "Price: " << pair.second.price << std::endl;
-        std::cout << "Quantity: " << pair.second.quantity << std::endl;
+        cout << "Stock name: " << pair.first << endl;
+        cout << "Open: " << pair.second.open << endl;
+        cout << "High: " << pair.second.high << endl;
+        cout << "Low: " << pair.second.low << endl;
+        cout << "Close: " << pair.second.close << endl;
+        cout << "Volume: " << pair.second.volume << endl;
+        cout << "Dividends: " << pair.second.dividends << endl;
         // Access other members of StockInfo struct as needed
     }
 
